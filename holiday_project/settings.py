dbd721e248c18e89e92ae84d1ac586cc31198a13
Odunlola <odunolaodunlami@gmail.com>
2023-07-18 22:53:51 -0500

    .

--- holiday_project/settings.py
+++ holiday_project/settings.py
@@ -41,7 +41,6 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Application definition
 
 INSTALLED_APPS = [
-    'render.apps.RenderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
