23fe1e5f1bbe006e862d8594ed4c4b13dc55013f
Odunlola <odunolaodunlami@gmail.com>
2023-07-20 19:58:29 -0500

    .

--- holiday_project/settings.py
+++ holiday_project/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
 
-    # 'django.contrib.sites',
+    'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount', 
